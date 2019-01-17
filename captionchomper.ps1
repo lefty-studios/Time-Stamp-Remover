e2386b462afc65d6a35023a33c6170e7716fb820
lefty-studios <lefty-studios@users.noreply.github.com>
2019-01-17 10:05:11 -0500

    Update captionchomper.ps1
    Now runs through a entire folder and trims the timestamps

--- captionchomper.ps1
+++ captionchomper.ps1
@@ -1,13 +1,13 @@
-﻿$path = "C:\Users\notmyaccountusername\Desktop\captions.sbv"
-function FormatSBV {
-    $i = 1
-    (gc $path) | ? {$_.trim() -ne "" } | set-content $path
-    Get-Content $path | Where-Object { $i % 2 -eq 0; $i++ }
-    Set-Content -Path $path -Value (Get-Content $path | Where-Object { $i % 2 -eq 0; $i++ })
+$folderpath = "C:\Users\amhadinger\Desktop\captions\"
+function FormatCaptions {
+    ForEach ($path in Get-ChildItem -Path $folderpath -Recurse -Include *.sbv) {
+        $i = 1
+        (gc $path) | ? {$_.trim() -ne "" } | set-content $path
+        Get-Content $path | Where-Object { $i % 2 -eq 0; $i++ }
+        Set-Content -Path $path -Value (Get-Content $path | Where-Object { $i % 2 -eq 0; $i++ })
+        $name = (Get-Item $path).Basename
+        $newFile = $folderpath + $name + ".txt"
+        Get-Content $path | Out-File -FilePath $newFile
+    }
 }
-function GetTXT {
-    $path1 = "C:\Users\notmyusername\Desktop\captions.txt"
-    Get-Content $path | Out-File -FilePath $path1
-}
-FormatSBV
-GetTXT
+FormatCaptions
